a5ce68a965830d079d7e648641963bce31c08b89
Maia Manze <142365480+maiamanze@users.noreply.github.com>
2023-10-16 18:02:05 -0300

    db final

--- tpe.sql
+++ tpe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-10-2023 a las 00:14:09
+-- Tiempo de generación: 16-10-2023 a las 17:20:51
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -24,15 +24,24 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `compras`
+-- Estructura de tabla para la tabla `categoria`
 --
 
-CREATE TABLE `compras` (
+CREATE TABLE `categoria` (
   `id` int(11) NOT NULL,
-  `producto` int(11) NOT NULL,
-  `monto` double NOT NULL,
-  `cliente` int(11) NOT NULL
+  `tipo` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`id`, `tipo`) VALUES
+(1, 'Exterior'),
+(2, 'Cocina'),
+(3, 'Baño'),
+(4, 'Oficina'),
+(5, 'Interior');
 
 -- --------------------------------------------------------
 
@@ -45,8 +54,35 @@ CREATE TABLE `productos` (
   `nombre` varchar(200) NOT NULL,
   `material` varchar(200) NOT NULL,
   `precio` double NOT NULL,
-  `cantidad` int(11) NOT NULL
+  `imagen` varchar(1000) NOT NULL,
+  `categoria` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `nombre`, `material`, `precio`, `imagen`, `categoria`) VALUES
+(4, 'Silla oficina', 'Plastico', 8750, 'https://http2.mlstatic.com/D_NQ_NP_601919-MLA52213058676_102022-O.webp', 4),
+(6, 'Tocador doble', 'Marmol', 16500, 'https://hips.hearstapps.com/es.h-cdn.co/mcres/images/mi-casa/banos/tocador-doble/1316665-2-esl-ES/tocador-doble.jpg?resize=640:*', 3),
+(7, 'Tocador simple', 'Marmol', 13800, 'https://www.elmueble.com/medio/2023/02/27/bano-con-ducha-y-mueble-volado-blanco_00000000_00495436_230227225011_600x600.jpg', 3),
+(11, 'Reposera de playa', 'Metal', 8000, 'https://http2.mlstatic.com/D_NQ_NP_2X_876334-MLU71211856642_082023-F.webp', 1),
+(13, 'Reposera doble', 'Aluminio', 30000, 'https://http2.mlstatic.com/D_NQ_NP_2X_805418-MLA69553507784_052023-F.webp', 1),
+(14, 'asasd', 'Madera', 89000, 'https://http2.mlstatic.com/D_NQ_NP_2X_787937-MLA43407388198_092020-F.webp', 3),
+(15, 'Organizador de Baño', 'Madera', 16000, 'https://http2.mlstatic.com/D_NQ_NP_2X_601909-MLA71086972510_082023-F.webp', 3),
+(16, 'Organizador de baño', 'Aluminio', 12800, 'https://http2.mlstatic.com/D_NQ_NP_2X_760338-MLA71826850502_092023-F.webp', 3),
+(17, 'Mesa de comedor Simple', 'Madera', 16780, 'https://http2.mlstatic.com/D_NQ_NP_2X_767044-MLA71125146869_082023-F.webp', 2),
+(18, 'Alacena 2 Puertas', 'Madera', 10999, ' https://http2.mlstatic.com/D_NQ_NP_2X_804487-MLA70002035183_062023-F.webp', 2),
+(20, 'Organizador colgante', 'Madera', 13500, 'https://http2.mlstatic.com/D_NQ_NP_2X_816150-MLU71139771797_082023-F.webp', 3),
+(21, 'Tacho de basura', 'Aluminio', 8900, 'https://http2.mlstatic.com/D_NQ_NP_2X_660724-MLA71954567485_092023-F.webp', 2),
+(22, 'Sofa esquienro', 'Madera/lona', 87000, 'https://http2.mlstatic.com/D_NQ_NP_2X_807117-MLA51739692118_092022-F.webp', 5),
+(23, 'Sillon individual', 'Madera/Lona', 28000, 'https://http2.mlstatic.com/D_NQ_NP_2X_892538-MLA71898784946_092023-F.webp', 5),
+(28, 'Estanteria Libros', 'Madera', 13000, 'https://http2.mlstatic.com/D_NQ_NP_2X_682539-MLA44678279022_012021-F.webp', 4),
+(29, 'Hamaca paraguaya', 'tela', 9999, 'https://http2.mlstatic.com/D_NQ_NP_2X_726973-MLA53226764822_012023-F.webp', 1),
+(30, 'Combo 6 sillas y mesa', 'plastico', 40000, 'https://http2.mlstatic.com/D_NQ_NP_2X_943509-MLA71435186403_082023-F.webp', 1),
+(32, 'Escritorio y silla oficina', 'Aluminio', 56000, 'https://http2.mlstatic.com/D_NQ_NP_2X_908293-MLA53966224426_022023-F.webp', 4),
+(33, 'Placard ', 'Madera', 23800, 'https://http2.mlstatic.com/D_NQ_NP_2X_670742-MLA52347344819_112022-F.webp', 5),
+(34, 'asdasd', 'Madera', 80000, 'https://http2.mlstatic.com/D_NQ_NP_2X_660914-MLA49951522983_052022-F.webp', 5);
 
 -- --------------------------------------------------------
 
@@ -62,22 +98,29 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `email`, `password`, `admin`) VALUES
+(16, 'webadmin@gmail.com', '$2y$10$86Byn3XALNtLZjd84kcOZesKnFOA3WtqY6TsQ2aCx5c4WHJf92I8m', 1),
+(18, 'maia.manze@gmail.com', '$2y$10$J315ytKj5WJyR8l7vY3p..VzVy3MpF6Diie/Vwy3kENJ5XNZqrT0a', 0);
+
+--
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `compras`
+-- Indices de la tabla `categoria`
 --
-ALTER TABLE `compras`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_cliente` (`cliente`),
-  ADD KEY `id_producto` (`producto`);
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `productos`
 --
 ALTER TABLE `productos`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `categoria_fk` (`categoria`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -90,36 +133,34 @@ ALTER TABLE `usuarios`
 --
 
 --
--- AUTO_INCREMENT de la tabla `compras`
+-- AUTO_INCREMENT de la tabla `categoria`
 --
-ALTER TABLE `compras`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+ALTER TABLE `categoria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `compras`
+-- Filtros para la tabla `productos`
 --
-ALTER TABLE `compras`
-  ADD CONSTRAINT `fk_cliente` FOREIGN KEY (`cliente`) REFERENCES `usuarios` (`id`),
-  ADD CONSTRAINT `id_producto` FOREIGN KEY (`producto`) REFERENCES `productos` (`id`);
+ALTER TABLE `productos`
+  ADD CONSTRAINT `categoria_fk` FOREIGN KEY (`categoria`) REFERENCES `categoria` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
